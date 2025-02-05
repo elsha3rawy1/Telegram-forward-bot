e9039b85183880ac660aab7d145151eca9e95cfc
elsha3rawy1 <167137451+elsha3rawy1@users.noreply.github.com>
2025-02-05 14:05:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
-from telethon import TelegramClient, events
+from telethon.sync import TelegramClient, events
 import asyncio
 
-api_id = '26283926'
-api_hash = 'fcd8c080125fad9062c9bc7d9cb2ca2d'
-phone_number = '+201093706011'
-
-source_channel = 'hhahsgsuw6'
-target_channel = 'signals_elsha3rawy1'
-
-owner_id = 2065056216
+api_id = 26283926  
+api_hash = 'fcd8c080125fad9062c9bc7d9cb2ca2d'  
+source_channel = -1002279845213  
+destination_bot = -1002304519486
 
 client = TelegramClient('session_name', api_id, api_hash)
 
 @client.on(events.NewMessage(chats=source_channel))
-async def handler(event):
-    message_text = event.message.text
-    sender_id = event.sender_id
-    if sender_id == owner_id and message_text.endswith("pump"):
-        await client.forward_messages(target_channel, event.message)
+async def forward_message(event):
+    print(f"Received Message: {event.message.text}")  
+    try:
+        await client.send_message(destination_bot, event.message.text)  
+    except Exception as e:
+        print(f"Error sending message: {e}")
 
-async def main():
-    await client.start(phone_number)
-    await client.run_until_disconnected()
+async def restart_client():
+    while True:
+        try:
+            print("إعادة الاتصال بتليغرام...")
+            await client.connect()
+            if not await client.is_user_authorized():
+                print("الجلسة غير مصرح بها، تأكد من تسجيل الدخول.")
+                return
+            print("تم الاتصال بنجاح!")
+            await client.run_until_disconnected()
+        except Exception as e:
+            print(f"فشل الاتصال، إعادة المحاولة بعد 5 ثوانٍ... {e}")
+            await asyncio.sleep(5)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+print("البوت يعمل الآن...")
+client.loop.run_until_complete(restart_client())
